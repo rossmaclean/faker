e38cc03d828396f4f8c15446f1164c7c22e67067
Ross MacLean <ross@rossmac.co.uk>
2022-07-19 11:33:14 +0100

    Add chown.

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
+chown -R cloudron:cloudron /app/code
+
 echo "Starting Faker"
-ls -la /app/pkg
-ls -la /app/code
-ls -la /app/code/api
-/app/code/api/faker
-#exec /usr/local/bin/gosu cloudron:cloudron /app/code/api/faker
+exec /usr/local/bin/gosu cloudron:cloudron /app/code/api/faker
