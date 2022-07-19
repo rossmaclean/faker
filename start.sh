84d62f75f7244871c678aa4c22f9373765775657
Ross MacLean <ross@rossmac.co.uk>
2022-07-19 11:28:19 +0100

    Add logging.

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "Starting Faker"
-exec /usr/local/bin/gosu cloudron:cloudron /app/code/api/faker
+ls -la /app/pkg
+ls -la /app/code
+ls -la /app/code/api
+/app/code/api/faker
+#exec /usr/local/bin/gosu cloudron:cloudron /app/code/api/faker
